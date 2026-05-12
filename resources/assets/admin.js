b9b6513cced5ed6b1049cc1f2990e6e01e3bd0ec
Zdeněk Papučík <zdenek.papucik@gmail.com>
2026-05-12 12:19:54 +0200

    Update admin.js

--- resources/assets/admin.js
+++ resources/assets/admin.js
@@ -1,12 +1,5 @@
 // these JS + SCSS will be automatically available after installing the package
-import "./core/base.js";
-
-// registration naja extensions
-function registerExtensions(...extensions) {
-	extensions.forEach(Extension => {
-		naja.registerExtension(new Extension());
-	});
-}
+import {registerNajaExtensions} from "./core/base.js";
 
 registerExtensions(
 	// naja extensions
