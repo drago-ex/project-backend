fe242c466b1c2032e3f5f77a749e508f03606c95
accgit <zdenek.papucik@gmail.com>
2026-05-24 12:08:28 +0200

    Update

--- resources/app/UI/Backend/Admin/AdminPresenter.php
+++ resources/app/UI/Backend/Admin/AdminPresenter.php
@@ -8,12 +8,9 @@ use App\UI\BasePresenter;
 
 
 /**
- * AdminPresenter is responsible for handling the admin section of the application.
- * It extends the base Presenter class and is linked with the AdminTemplate for rendering.
- *
+ * Admin presenter.
  * @property-read AdminTemplate $template
  */
 final class AdminPresenter extends BasePresenter
 {
-	// This class is currently empty.
 }
--- resources/app/UI/Backend/Admin/AdminTemplate.php
+++ resources/app/UI/Backend/Admin/AdminTemplate.php
@@ -7,12 +7,7 @@ namespace App\UI\Backend\Admin;
 use App\UI\BaseTemplate;
 
 
-/**
- * AdminTemplate is used for rendering the admin section of the application.
- * It extends the base Template class to provide specific template functionality
- * for the admin section of the app.
- */
+/** Admin template. */
 class AdminTemplate extends BaseTemplate
 {
-	// There is nothing here yet.
 }
